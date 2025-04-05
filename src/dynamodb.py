ec3d471b144ea3bdeae8c7ebc430985d01c72daf
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-05 16:01:35 +0300

    Update table region

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -4,5 +4,5 @@ import settings
 
 def dynamodb_table(table_name: str):
     dynamodb = boto3.resource(
-        'dynamodb', endpoint_url=settings.DYNAMODB_URL_OVERRIDE)
+        'dynamodb', region_name=settings.DOWNTIMES_TABLE_REGION, endpoint_url=settings.DYNAMODB_URL_OVERRIDE)
     return dynamodb.Table(f"{settings.TABLE_PREFIX}.{table_name}")
--- src/settings.py
+++ src/settings.py
@@ -9,5 +9,6 @@ AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', '')
 DYNAMODB_URL_OVERRIDE = os.environ.get('DYNAMODB_URL_OVERRIDE')
 TABLE_PREFIX = os.environ.get(
     'TABLE_PREFIX') or "production" if ENVIRONMENT == "production" else "stage"
+DOWNTIMES_TABLE_REGION = "eu-central-1"
 DOWNTIMES_TABLE_NAME = "webeye.downtimes"
 DOWNTIMES_METATYPE_LSI = "metatype-lsi"
