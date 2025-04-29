10ef73c852e600ecae30d34e396372ee1a01ef3e
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-29 14:18:23 +0300

    Testing differences

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ TARGET_URL = "https://www.cs.ubbcluj.ro"
 
 if __name__ == "__main__":
     if settings.ENVIRONMENT == "test":
-        results = application_context.events.check_webpage("03715419-f79a-4826-8c5e-5e331777b1f4",
+        results = application_context.events.check_webpage("fb8b90bc-6015-4b46-8e6a-c2bb178d97f6",
                                                            TARGET_URL, True, "<!DOCTYPE html", [400, 500, 404], None)
 
         print(json.dumps(results["event"].to_db_item(), indent=2))
